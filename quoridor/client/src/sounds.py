5b5e341ce07b67f26de2152f61954f23a02a8f73
Quentin18 <quentindeschamps18@orange.fr>
2020-05-16 02:34:47 +0200

    Add sound + class Sounds

---
+++ quoridor/client/src/sounds.py
@@ -0,0 +1,14 @@
+"""
+Quoridor Online
+Quentin Deschamps, 2020
+"""
+import pygame
+
+
+class Sounds:
+    def __init__(self, path):
+        pygame.mixer.init()
+        self.start_sound = pygame.mixer.Sound(
+            "".join([path, "/sounds/start_sound.wav"]))
+        self.winning_sound = pygame.mixer.Sound(
+            "".join([path, "/sounds/winning_sound.wav"]))
